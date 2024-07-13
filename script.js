08de8b8235a5e6580f9bdbfbd771f1aeead4f58d
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-07-13 10:59:41 -0400

    search table

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ const search = document.querySelector('.input-group input'),
     table_rows = document.querySelectorAll('tbody tr'),
     table_headings = document.querySelectorAll('thead th');
 
+    const found = document.querySelector(".found");
+
 // 1. Searching for specific data of HTML table
 search.addEventListener('input', searchTable);
 
 function searchTable() {
     table_rows.forEach((row, i) => {
         let table_data = row.textContent.toLowerCase(),
-            search_data = search.value.toLowerCase();
-
+            search_data = search.value.toLowerCase(),
+            finds = table_data.indexOf(search_data);
+        console.log(finds);
         row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
         row.style.setProperty('--delay', i / 25 + 's');
+        if(finds <=0 ){
+            found.textContent = "no results found";
+        }
     })
+    
+    
 
     document.querySelectorAll('tbody tr:not(.hide)').forEach((visible_row, i) => {
         visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
@@ -41,10 +49,7 @@ table_headings.forEach((head, i) => {
             sort_asc = head.classList.contains('asc') ? false : true;
     
             sortTable(i, sort_asc);
-        
-        
     }
-    
 })
 
 
@@ -52,10 +57,7 @@ function sortTable(column, sort_asc) {
     [...table_rows].sort((a, b) => {
         let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase(),
             second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
-
-        //return compareValues(first_row,t2.second_row)
-        //return (a<b) ? -1 : (a>b) ? 1 : 0;
-        
+       
         return sort_asc ? (first_row < second_row ? 1 : -1) : (first_row < second_row ? -1 : 1);
     })
         .map(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
